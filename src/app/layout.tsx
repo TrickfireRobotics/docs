9694ce35f27b979ba470f2139f75bc2e732118f1
Matej Stastny <matysta@outlook.com>
2026-07-31 11:14:46 -0700

    fix: added meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,21 @@
+import type { Metadata } from "next";
 import { Overpass } from "next/font/google";
 import { Provider } from "@/components/provider";
+import { appName } from "@/lib/shared";
 import "./global.css";
 
 const overpass = Overpass({
     subsets: ["latin"],
     variable: "--font-overpass",
 });
+
+export const metadata: Metadata = {
+    title: {
+        default: appName,
+        template: `%s | ${appName}`,
+    },
+    description: "Documentation for TrickFire Robotics projects",
+};
 
 export default function Layout({ children }: LayoutProps<"/">) {
     return (
