7aeb3bbb4e2dbe2ca6dd0e96fd85549edfaba0c1
Matej Stastny <matysta@outlook.com>
2026-07-30 22:10:43 -0700

    fix: standardize dev docs

--- scripts/dev-link-docs.sh
+++ scripts/dev-link-docs.sh
@@ -4,10 +4,11 @@ set -e
 
 PROJECT_ROOT=$(realpath "$(dirname "${BASH_SOURCE[0]}")/..")
 PROJECT_DIR=$1
-LINK_PATH=$PROJECT_ROOT/content/$(basename $PROJECT_DIR)
+LINK_PATH="$PROJECT_ROOT/content/$(basename "$PROJECT_DIR")"
 
-if [ -z $PROJECT_DIR ]; then
+if [ -z "$PROJECT_DIR" ]; then
     echo "Usage: dev-link-docs <project_path>"
+    exit 1
 fi
 
 if [ ! -d "$PROJECT_DIR" ]; then
@@ -25,14 +26,13 @@ if [ ! -f "$PROJECT_DIR/docs.config.json" ]; then
     exit 1
 fi
 
-mkdir -p $LINK_PATH
+mkdir -p "$LINK_PATH"
 echo "==> Linking docs/"
-ln -s $PROJECT_DIR/docs $LINK_PATH/docs
+ln -s "$PROJECT_DIR/docs" "$LINK_PATH/docs"
 echo "==> Linking docs.config.json..."
-ln -s $PROJECT_DIR/docs.config.json $LINK_PATH/docs.config.json
+ln -s "$PROJECT_DIR/docs.config.json" "$LINK_PATH/docs.config.json"
 
 echo "==> Creating .gitignore..."
-touch $PROJECT_DIR/.gitignore
-echo "*" > $PROJECT_DIR/.gitignore
+printf '*' > "$LINK_PATH/.gitignore"
 
-echo "Symlink to docs created for project $(basename $PROJECT_DIR)"
+echo "Symlink to docs created for project $(basename "$PROJECT_DIR")"
