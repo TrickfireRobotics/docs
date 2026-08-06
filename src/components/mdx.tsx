a76a3e05a0c51164ca2b8dc04661ce339857dc6c
Matej Stastny <matysta@outlook.com>
2026-08-06 12:32:22 -0700

    feat: accordions

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -1,6 +1,7 @@
 import defaultMdxComponents from "fumadocs-ui/mdx";
 import { Step, Steps } from "fumadocs-ui/components/steps";
 import { Tab, Tabs } from "fumadocs-ui/components/tabs";
+import { Accordion, Accordions } from "fumadocs-ui/components/accordion";
 import type { MDXComponents } from "mdx/types";
 
 export function getMDXComponents(components?: MDXComponents) {
@@ -10,6 +11,8 @@ export function getMDXComponents(components?: MDXComponents) {
         Steps,
         Tab,
         Tabs,
+        Accordion,
+        Accordions,
         ...components,
     } satisfies MDXComponents;
 }
